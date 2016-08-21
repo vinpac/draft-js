caaf437fdd952355b403e587c19fa63aea132333
Vinicius Pacheco <vin175pacheco@gmail.com>
2016-08-21 18:55:26 -0300

    Fix restoring uncollapsed selection when re-focusing

--- src/component/handlers/edit/editOnFocus.js
+++ src/component/handlers/edit/editOnFocus.js
@@ -30,7 +30,11 @@ function editOnFocus(e: SyntheticFocusEvent): void {
   // moves the cursor back to the beginning of the editor, so we force the
   // selection here instead of simply accepting it in order to preserve the
   // old cursor position. See https://crbug.com/540004.
-  this.update(EditorState.forceSelection(editorState, selection));
+  if (currentSelection.isCollapsed()) {
+    this.update(EditorState.forceSelection(editorState, selection));
+  } else {
+    this.update(EditorState.acceptSelection(editorState, selection));
+  }
 }
 
 module.exports = editOnFocus;
